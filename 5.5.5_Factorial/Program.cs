506abab9486a0e17e746fde6f2a2bff390a334e2
Александр Полушкин <aa.polushkin@yandex.ru>
2022-12-14 10:33:30 +0300

    5.5.8 - Рекурсивный метод PowerUp возведения в степень

---
+++ 5.5.5_Factorial/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _5._5._5_Factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Factorial(20));
+
+            Console.WriteLine(PowerUp(2,4));
+        }
+        static decimal Factorial(int x)
+        {
+            if (x == 0)
+            {
+                return 1;
+            }
+            return x * Factorial(x - 1);
+        }
+        static int PowerUp(int N, byte pow)
+        {
+            if (pow == 0)
+            {
+                return 1;
+            }
+            else if (pow == 1)
+            {
+                return N;
+            }
+            return N * PowerUp(N,--pow);
+        }
+    }
+}
